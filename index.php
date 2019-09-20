dfed1056ede13e54f3aa1ab433b990760e2fd5c7
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-09-20 17:04:14 +0200

    aggiunto template

--- index.php
+++ index.php
@@ -13,6 +13,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.0/handlebars.min.js" charset="utf-8"></script>
     <!-- TEMPLATE-->
     <script id="item-template" type="text/x-handlebars-template">
+    <div class= 'data-cont'>
+      <p class='line-1'>{{riassunto}}</p>
+      <p class='line-2'>{{area}}</p>
+      <p class='line-3'>{{volume}}</p>
+    </div>
     </script>
 
     <!-- CSS: MY STYLE -->
@@ -20,13 +25,10 @@
     <!-- JS: MY SCRIPT -->
     <script src="script.js" charset="utf-8"></script>
     <title>Parallelepipedo</title>
-    <?php
-      include 'parallelepipedo.php';
-
-    ?>
   </head>
   <body>
   <div class="container">
+ 
   </div>
   </body>
 </html>
