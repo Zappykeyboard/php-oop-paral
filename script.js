3de72d1125671a1ae46739788ea70092d00d3984
Zappykeyboard <edoardo.dambrosio@gmail.com>
2019-09-20 17:30:10 +0200

    aggiunto funzione per query area/volume al server

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function init() {
   console.log("Hello World");
+  getResponse();
 }
 
 $(document).ready(init);
+
+
+function getResponse(){
+
+  $.ajax({
+    url: 'api.php',
+    method: 'GET',
+    data: {
+      paral1: [2, 3, 4],
+      paral2: [5,6,7],
+      paral3:[8,9,10]
+    },
+    success: function(data){
+      console.log(data);
+      
+    },
+    error: function(err){
+      console.log(err);
+    }
+
+  })
+}
